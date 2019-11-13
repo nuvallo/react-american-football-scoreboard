565fc4fd09ed52821fe77e6f7cc22227b6aa9158
Tyler <nuvallo.inc@gmail.com>
2019-11-13 15:29:04 -0500

    adding the correct score when scoring a touchdown

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, setHomeScore] = useState(0);
-  const [aweyScore, setAwayScore] = useState(0);
-
+  const [homeScore, setHomeScore] = useState(32);
+  const [awayScore, setAwayScore] = useState(32);
+  const addHomeScore = () => {
+    setHomeScore(homeScore + 6);
+  };
+  const addAwayScore = () => {
+    setAwayScore(awayScore + 6);
+  };
   return (
     <div className="container">
       <section className="scoreboard">
@@ -22,7 +27,7 @@ function App() {
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{aweyScore}</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -30,11 +35,15 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={addHomeScore} className="homeButtons__touchdown">
+            Home Touchdown
+          </button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={addAwayScore} className="awayButtons__touchdown">
+            Away Touchdown
+          </button>
           <button className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
